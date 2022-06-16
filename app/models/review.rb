3e925f9d22fb0b9144d7d5ae7fc4f4fb0e13ae00
NicMortelliti <npmortelliti@gmail.com>
2022-06-16 10:05:24 -0700

    commit tree 1e61433ead2a6d65ae78136b97a2728c9c005120

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,11 @@
 class Review < ActiveRecord::Base
+  # # a review belongs to a game
+  # def game
+  #   # self is the review instance
+  #   Game.find(self.game_id)
+  # end  
   
+  # The below line does the job of all
+  # of the code above...
+  belongs_to :game
 end
